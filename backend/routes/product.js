fc1c9211e6150fcae2cbec48d6fd080a1ede885d
Jules-Rabus <jules200204@gmail.com>
2024-05-25 15:29:44 +0200

    Add product to a category

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,82 +3,100 @@ const { Product, Category, Image } = require("../models");
 const router = new Router();
 
 router.get("/", async (req, res) => {
-  const products = await Product.findAll({
-    where: req.query,
-    include: [Category, Image],
-  });
-  res.json(products);
+    const products = await Product.findAll({
+        where: req.query,
+        include: [Category, Image],
+    });
+    res.json(products);
 });
 
 router.post("/", async (req, res, next) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
-  } catch (e) {
-    next(e);
-  }
+    try {
+        const { Categories, ...productData } = req.body;
+        const product = await Product.create(productData);
+
+        if (Categories && Categories.length) {
+            const categories = await Category.findAll({ where: { id: Categories } });
+            await product.setCategories(categories);
+        }
+
+        res.status(201).json(product);
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.get("/:id", async (req, res, next) => {
-  try {
-    const product = await Product.findByPk(parseInt(req.params.id));
-    if (Product) {
-      res.json(product);
-    } else {
-      res.sendStatus(404);
+    try {
+        const product = await Product.findByPk(parseInt(req.params.id), { include: [Category, Image] });
+        if (product) {
+            res.json(product);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (e) {
+        next(e);
     }
-  } catch (e) {
-    next(e);
-  }
 });
 
 router.patch("/:id", async (req, res, next) => {
-  try {
-    const [nbUpdated, products] = await Product.update(req.body, {
-      where: {
-        id: parseInt(req.params.id),
-      },
-      returning: true,
-    });
-    if (nbUpdated === 1) {
-      res.json(products[0]);
-    } else {
-      res.sendStatus(404);
+    try {
+        const { Categories, ...productData } = req.body;
+        const product = await Product.findByPk(parseInt(req.params.id));
+
+        if (product) {
+            await product.update(productData);
+
+            if (Categories && Categories.length) {
+                const categories = await Category.findAll({ where: { id: Categories } });
+                await product.setCategories(categories);
+            }
+
+            res.json(product);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (e) {
+        next(e);
     }
-  } catch (e) {
-    next(e);
-  }
 });
 
 router.delete("/:id", async (req, res, next) => {
-  try {
-    const nbDeleted = await Product.destroy({
-      where: {
-        id: parseInt(req.params.id),
-      },
-    });
-    if (nbDeleted === 1) {
-      res.sendStatus(204);
-    } else {
-      res.sendStatus(404);
+    try {
+        const nbDeleted = await Product.destroy({
+            where: {
+                id: parseInt(req.params.id),
+            },
+        });
+        if (nbDeleted === 1) {
+            res.sendStatus(204);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (e) {
+        next(e);
     }
-  } catch (e) {
-    next(e);
-  }
 });
 
 router.put("/:id", async (req, res, next) => {
-  try {
-    const nbDeleted = await Product.destroy({
-      where: {
-        id: parseInt(req.params.id),
-      },
-    });
-    const product = await Product.create(req.body);
-    res.status(nbDeleted ? 200 : 201).json(product);
-  } catch (e) {
-    next(e);
-  }
+    try {
+        const { Categories, ...productData } = req.body;
+        await Product.destroy({
+            where: {
+                id: parseInt(req.params.id),
+            },
+        });
+        const product = await Product.create(productData);
+
+        if (Categories && Categories.length) {
+            const categories = await Category.findAll({ where: { id: Categories } });
+            await product.setCategories(categories);
+        }
+
+        res.status(200).json(product);
+    } catch (e) {
+        next(e);
+    }
 });
 
 module.exports = router;
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -49,7 +49,11 @@ const productSchema = z.object({
   reference: z.string().min(1, "Reference is required"),
   price: z.number().min(0, "Price must be a positive number"),
   active: z.boolean(),
-  description: z.string().optional()
+  description: z.string().optional(),
+  Categories: z.array(z.number()).optional(),
+  Images: z.array(z.number()).optional(),
+  createdAt: z.string().time().optional(),
+  updateTimestamp: z.string().time().optional()
 });
 
 const products = ref([]);
@@ -58,7 +62,8 @@ const newProduct = ref({
   reference: '',
   price: 0,
   active: false,
-  description: ''
+  description: '',
+  Categories: [],
 });
 
 // Function to fetch products
@@ -75,6 +80,7 @@ const fetchProducts = async () => {
 const addProduct = async () => {
   try {
     const parsedProduct = productSchema.parse(newProduct.value);
+    console.log(parsedProduct);
     const response = await axios.post('http://localhost:3000/products', parsedProduct);
     products.value.push(response.data);
     newProduct.value = {
@@ -82,12 +88,35 @@ const addProduct = async () => {
       reference: '',
       price: 0,
       active: false,
-      description: ''
+      description: '',
+      Categories: [],
     };
   } catch (error) {
     console.error('Error adding product:', error);
   }
 };
+
+// Function to modify a product
+const modifyProduct = async (product) => {
+  try {
+    console.log(product);
+    //const parsedProduct = productSchema.parse(product);
+    await axios.put(`http://localhost:3000/products/${product.id}`, product);
+  } catch (error) {
+    console.error('Error modifying product:', error);
+  }
+};
+
+// Function to delete a product
+const deleteProduct = async (product) => {
+  try {
+    await axios.delete(`http://localhost:3000/products/${product.id}`);
+    products.value = products.value.filter((p) => p.id !== product.id);
+  } catch (error) {
+    console.error('Error deleting product:', error);
+  }
+};
+
 
 // Fetch products and categories when the component is mounted
 onMounted(() => {
@@ -101,29 +130,7 @@ onMounted(() => {
   <div class="products">
     <div> 
       <h1>Products</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Reference</th>
-            <th>Price</th>
-            <th>Active</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="product in products" :key="product.id">
-            <td>{{ product.name }}</td>
-            <td>{{ product.reference }}</td>
-            <td>{{ product.price }}</td>
-            <td>{{ product.active }}</td>
-            <td>{{ product.description }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <div class="addProducts">
+      <div class="addProducts">
       <h2>Add Products</h2>
       <form @submit.prevent="addProduct">
         <label for="name">Name</label>
@@ -133,7 +140,7 @@ onMounted(() => {
         <input v-model="newProduct.reference" type="text" id="reference" name="reference" required>
 
         <label for="price">Price</label>
-        <input v-model="newProduct.price" type="number" id="price" name="price" required>
+        <input v-model="newProduct.price" type="number" id="price" name="price" min="0" required>
 
         <label for="active">Active</label>
         <input v-model="newProduct.active" type="checkbox" id="active" name="active">
@@ -141,8 +148,48 @@ onMounted(() => {
         <label for="description">Description</label>
         <textarea v-model="newProduct.description" id="description" name="description"></textarea>
 
+        <label for="categories">Categories</label>
+        <select id="categories" name="categories" v-model="newProduct.Categories" multiple>
+          <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+        </select>
+
         <button type="submit">Add Product</button>
       </form>
+    </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Reference</th>
+            <th>Price</th>
+            <th>Active</th>
+            <th>Description</th>
+            <th>Categories</th>
+            <th>RAW DATA</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <td> <input v-model="product.name" type="text" required> </td>
+            <td> <input v-model="product.reference" type="text" required> </td>
+            <td> <input v-model.number="product.price" type="number" required min="0"> </td>
+            <td> <input v-model="product.active" type="checkbox"> </td>
+            <td> <input v-model="product.description" type="text"> </td>
+            <td>
+              <select v-model="product.Categories" multiple>
+                <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+              </select>
+            </td>
+            <td> {{ product}} </td>
+            <td>
+              <button @click="modifyProduct(product)">Modify</button>
+              <button @click="deleteProduct(product)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
   <div class="categories">
@@ -178,8 +225,18 @@ onMounted(() => {
     </div>
 
   </div>
+
 </template>
 
 <style scoped>
-/* Add your styles here */
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  th, td {
+    border: 1px solid black;
+    padding: 4px;
+  }
+
 </style>

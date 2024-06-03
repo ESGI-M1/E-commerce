405a40e767ffd6bbebcc13bff9fae1b392cbe866
melvin-crypto <melvin.pierre.mp@gmail.com>
2024-06-03 19:15:18 +0200

    Suppresion lien inutile route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 import Dashboard from '../views/Dashboard.vue';
 import About from '../views/About.vue';
 import Login from '../views/Login.vue';

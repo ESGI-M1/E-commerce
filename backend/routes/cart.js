fa6dc6608a30e39e2848d9e07f42588e151afa2e
melvin pierre <melvin.pierre.mp@gmail.com>
2024-06-12 14:51:38 +0200

    Ajout fonctionnalité panier non connecté

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,28 +1,81 @@
 const { Router } = require("express");
-const { Cart, Product } = require("../models");
+const { Cart, Product, User } = require("../models");
+const jwt = require('jsonwebtoken'); // Assurez-vous d'importer jwt pour décoder le token
 const router = new Router();
-const { authenticateUser } = require('../middleware/auth');
+const crypto = require('crypto'); // Importer le module crypto pour générer des mots de passe aléatoires
 
 router.get("/", async (req, res, next) => {
   try {
-    const userId = req.user.id;
-    const cartItems = await Cart.findAll({ where: { userId }, include: [Product] });
+    const cartItems = await Cart.findAll({ include: [{ model: Product, as: 'product' }] });
+    console.log("Cart items:", cartItems); // Ajoutez ce log pour vérifier les données récupérées
     res.json(cartItems);
   } catch (error) {
     console.error('Error fetching cart items:', error);
-    res.status(500).json({ error: 'Unable to fetch cart items' });
+    res.status(500).json({ error: 'Unable to fetch cart items', details: error.message });
   }
 });
 
-router.post("/", authenticateUser, async (req, res, next) => {
-  const { productId, quantity } = req.body;
-  const userId = req.user.id;
 
+// Fonction pour générer un mot de passe aléatoire qui répond aux critères de validation
+function generateRandomPassword(length) {
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  const specialChars = '!@#$%^&*()-_=+[{]}|;:,<.>/?';
+
+  // Concaténer tous les caractères autorisés dans le mot de passe
+  const allChars = lowercaseLetters + uppercaseLetters + numbers + specialChars;
+
+  let password = '';
+  let charTypesCount = 0;
+
+  // Générer un mot de passe jusqu'à ce qu'il réponde à tous les critères de validation
+  while (password.length < length || charTypesCount < 4) {
+    password = '';
+    charTypesCount = 0;
+
+    // Ajouter des caractères aléatoires au mot de passe
+    for (let i = 0; i < length; i++) {
+      const randomIndex = crypto.randomInt(allChars.length);
+      password += allChars[randomIndex];
+    }
+
+    // Vérifier la présence des différents types de caractères dans le mot de passe
+    if (/[a-z]/.test(password)) charTypesCount++;
+    if (/[A-Z]/.test(password)) charTypesCount++;
+    if (/\d/.test(password)) charTypesCount++;
+    if (/[^a-zA-Z\d]/.test(password)) charTypesCount++;
+  }
+
+  return password;
+}
+
+router.post("/", async (req, res, next) => {
   try {
+    let userId = req.body.userId;
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const existingCart = await Cart.findOne({ where: { userId } });
+    if (!existingCart) {
+        const tempPassword = generateRandomPassword(12);
+        const tempUser = await User.create({
+          id: userId,
+          firstname: 'Temp',
+          lastname: 'User',
+          email: 'temporary@example.com',
+          password: tempPassword,
+          role: 'temp'
+        });
+    }
+
+    let productId = req.body.productId;
+    let quantity = 1;
     const existingCartItem = await Cart.findOne({ where: { userId, productId } });
 
     if (existingCartItem) {
-      existingCartItem.quantity += quantity;
+      existingCartItem.quantity += 1;
       await existingCartItem.save();
     } else {
       await Cart.create({ userId, productId, quantity });
@@ -35,8 +88,27 @@ router.post("/", authenticateUser, async (req, res, next) => {
   }
 });
 
-router.delete("/:id", authenticateUser, async (req, res, next) => {
-  const userId = req.user.id;
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+
+    const cartItems = await Cart.findAll({ where: { userId }, include: [{ model: Product, as: 'product' }] });
+
+    if (!cartItems || cartItems.length === 0) {
+      return res.status(404).json({ error: 'Cart not found' });
+    }
+
+    res.json(cartItems);
+  } catch (error) {
+    console.error('Error fetching cart details:', error);
+    res.status(500).json({ error: 'Unable to fetch cart details' });
+  }
+});
+
+
+
+router.delete("/:id", async (req, res, next) => {
+  const userId = req.query.userId;
   const cartItemId = req.params.id;
 
   try {
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -5,40 +5,50 @@
       <p><router-link to="/login">Rejoins-nous</router-link> ou <router-link to="/signup">S'identifier</router-link></p>
     </header>
 
-    <div class="cart-items" v-if="cartItems.length > 0">
-      <div v-for="(item, index) in cartItems" :key="index" class="cart-item">
-        <img :src="item.product.image" :alt="item.product.name">
-        <div class="item-details">
-          <h3>{{ item.product.name }}</h3>
-          <p>Prix: {{ item.product.price }} €</p>
-          <p>Quantité: {{ item.quantity }}</p>
+    <div class="cart-content">
+      <div class="cart-items" v-if="cartItems.value">
+        <div v-for="(item, index) in cartItems.value" :key="index" class="cart-item">
+          <div class="item-details">
+            <img :src="item.image[0].url ?? '../../produit_avatar.jpg'" :alt="item.image[0].description" class="product-image" />
+            <h3>{{ item.product.name }}</h3>
+          </div>
+          <div class="item-price">
+            <p>{{ item.product.price }} €</p>
+            <p>Quantité: {{ item.quantity }}</p>
+            <p>Total: {{ (item.product.price * item.quantity).toFixed(2) }} €</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      <p>Il n'y a aucun article dans ton panier.</p>
-    </div>
+      <div v-else>
+        <p>Il n'y a aucun article dans ton panier.</p>
+      </div>
 
-    <div class="cart-summary" v-if="cartItems.length > 0">
-      <div class="promo-code">
-        <label for="promo">As-tu un code promo ?</label>
-        <input type="text" id="promo" placeholder="Entrez votre code promo">
+      <div class="cart-summary" v-if="cartItems.value">
+        <h2>Récapitulatif</h2>
+        <div class="promo-code">
+          <label for="promo">As-tu un code promo ?</label>
+          <div class="promo-input">
+            <input type="text" id="promo" placeholder="Entrez votre code promo">
+            <button @click="applyPromoCode" class="apply-button">Appliquer</button>
+          </div>
+        </div>
+        <div class="totals">
+          <div class="subtotal">
+            <p>Sous-total</p>
+            <p>{{ subtotal }} €</p>
+          </div>
+          <div class="shipping">
+            <p>Frais estimés de prise en charge et d'expédition</p>
+            <p>Gratuit</p>
+          </div>
+          <div class="total">
+            <p>Total</p>
+            <p>{{ total }} €</p>
+          </div>
+        </div>
+        <button @click="checkout" class="checkout-button">Paiement</button>
+        <button @click="checkoutWithPaypal" class="paypal-button">Paiement avec PayPal <i class="fab fa-paypal"></i></button>
       </div>
-      <div class="totals">
-        <div class="subtotal">
-          <p>Sous-total</p>
-          <p>{{ cartSubtotal }} €</p>
-        </div>
-        <div class="shipping">
-          <p>Frais estimés de prise en charge et d'expédition</p>
-          <p>Gratuit</p>
-        </div>
-        <div class="total">
-          <p>Total</p>
-          <p>{{ cartTotal }} €</p>
-        </div>
-      </div>
-      <button @click="checkout">Paiement <i class="fab fa-paypal"></i></button>
     </div>
   </div>
 </template>
@@ -52,15 +62,25 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 const { cartItems, cartTotal, cartSubtotal } = useCartStore();
 
-
 const fetchCartItems = async () => {
   try {
-    const response = await axios.get('http://localhost:3000/cart', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
+    const temporaryId = localStorage.getItem('temporaryId');
+    if (temporaryId) {
+      const response = await axios.get(`http://localhost:3000/carts/${temporaryId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      console.log('Cart items:', response.data); // Vérifier la structure des données
+      cartItems.value = response.data;
+           for (const item of cartItems.value) {
+        const productId = item.productId;
+        const imageResponse = await axios.get(`http://localhost:3000/products/${productId}/images`);
+        const image = imageResponse.data;
+
+        item.image = image; // Supposons que l'image soit stockée dans la clé "image"
       }
-    });
-    cartItems.value = response.data;
+    }
   } catch (error) {
     console.error('Error fetching cart items:', error);
   }
@@ -75,7 +95,15 @@ const total = computed(() => {
 });
 
 const checkout = () => {
+  alert('Paiement effectué.');
+};
+
+const checkoutWithPaypal = () => {
   alert('Paiement effectué via PayPal.');
+};
+
+const applyPromoCode = () => {
+  alert('Code promo appliqué.');
 };
 
 onMounted(() => {
@@ -83,17 +111,21 @@ onMounted(() => {
 });
 </script>
 
-
 <style scoped>
-/* Styles CSS ici */
 .cart {
-  max-width: 800px;
+  max-width: 1200px;
   margin: 0 auto;
   padding: 20px;
 }
 
 header {
   margin-bottom: 20px;
+}
+
+.cart-content {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 20px;
 }
 
 .cart-items {
@@ -105,23 +137,49 @@ header {
   display: flex;
   align-items: center;
   margin-bottom: 20px;
+  background-color: #f9f9f9;
+  padding: 10px;
+  border-radius: 8px;
 }
 
-.cart-item img {
+.product-image {
   width: 100px;
+  height: 100px;
+  object-fit: cover;
   margin-right: 20px;
+}
+
+.item-details {
+  flex: 1;
 }
 
 .item-details h3 {
   margin: 0;
 }
 
+.item-price {
+  text-align: right;
+}
+
 .cart-summary {
-  margin-top: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: #f9f9f9;
+}
+
+.cart-summary h2 {
+  margin-top: 0;
 }
 
 .promo-code {
   margin-bottom: 20px;
+}
+
+.promo-input {
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 
 input[type="text"] {
@@ -132,27 +190,59 @@ input[type="text"] {
   margin-top: 5px;
 }
 
+.apply-button {
+  padding: 10px 20px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.apply-button:hover {
+  background-color: #f0f0f0;
+}
+
 .totals {
+  margin-bottom: 20px;
+}
+
+.totals > div {
   display: flex;
   justify-content: space-between;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
 }
 
 .total p {
   font-weight: bold;
 }
 
-button {
+.checkout-button {
   padding: 10px 20px;
-  background-color: #42b983;
+  background-color: #000;
   border: none;
   border-radius: 4px;
   color: white;
   cursor: pointer;
+  width: 100%;
+  margin-bottom: 10px;
 }
 
-button:hover {
-  background-color: #36a67d;
+.checkout-button:hover {
+  background-color: #333;
+}
+
+.paypal-button {
+  padding: 10px 20px;
+  background-color: #0070ba;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  width: 100%;
+}
+
+.paypal-button:hover {
+  background-color: #005ea6;
 }
 
 .fa-paypal {

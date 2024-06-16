2f322c3bb357660682da0aec0ac6b3bfb4a45365
melvin pierre <melvin.pierre.mp@gmail.com>
2024-06-16 18:14:22 +0200

    Ajout template users

---
+++ src/views/Ressources.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="resource-item" @click="navigateToUsers">
+      <h1>Ressources (admin)</h1>
+      <div class="user-card">
+        <h2>{{ username }}</h2>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useRouter } from 'vue-router';
+  
+  const username = "Utilisateur"; // Remplacez par la variable ou propriété contenant le nom de l'utilisateur
+  
+  const router = useRouter();
+  
+  const navigateToUsers = () => {
+    router.push('/users'); // Redirection vers la route '/users' lors du clic sur l'élément
+  };
+  </script>
+  
+  <style scoped>
+.user-card {
+  background: white;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  width: calc(33.333% - 20px);
+  cursor: pointer;
+  transition: transform 0.2s;
+  margin: 10px;
+}
+
+.user-card:hover {
+  transform: scale(1.05);
+}
+  </style>
+  

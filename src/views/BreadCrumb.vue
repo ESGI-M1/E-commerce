1e2a32b81a51657cbcea2ae00614d6154d2f978c
Jules Rabus <jules200204@gmail.com>
2024-07-25 08:04:37 +0000

    Fix BreadCrumb

--- src/views/BreadCrumb.vue
+++ src/views/BreadCrumb.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 import BreadCrumbLink from './BreadCrumbLink.vue'
+import { useRoute } from 'vue-router'
 
-defineProps({
+const props = defineProps({
   category: {
-    type: Object,
+    type: [Number, Object],
     required: true
   }
+})
+
+const category = ref(null)
+const route = useRoute()
+
+const fetchCategory = async (id: number) => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/categories/${id}`)
+    category.value = response.data
+  } catch (error) {
+    console.error('Error fetching category:', error)
+  }
+}
+
+const processCategory = () => {
+  if (typeof props.category === 'number') {
+    fetchCategory(props.category)
+  } else {
+    category.value = props.category
+  }
+}
+
+onMounted(() => {
+  processCategory()
 })
 </script>
 
 <template>
-  <div>
+  <div v-if="category">
     <span><router-link to="/">Home</router-link></span>
     <BreadCrumbLink :categoryId="category.parentCategoryId" v-if="category.parentCategoryId" />
     <span> > </span>
-    <RouterLink :to="{ name: 'Category', params: { slug: category.slug } }">{{
-      category.name
-    }}</RouterLink>
+    <router-link :to="{ name: 'Category', params: { slug: category.slug } }">
+      {{ category.name }}
+    </router-link>
   </div>
 </template>
